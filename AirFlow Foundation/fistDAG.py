0886384be67d87f92ce3e18f36a46e30ab3e0723
Jinyu Liu <ojin0116@gmail.com>
2024-10-27 00:31:15 -0400

    change

--- AirFlow Foundation/fistDAG.py
+++ AirFlow Foundation/fistDAG.py
@@ -1,7 +1,25 @@
 from airflow import DAG
+from airflow.operator.bash import BashOperator
+from datetime import datetime, timedelta
+
+args = {
+    'owner' : 'coder2j',
+    'retries' : 3,
+    'retry_delay' : timedelta(minutes=1)
+}
+
 
 with DAG(
     dag_id = 'our_first_dag',
-    description = 'This is our first dag that wewrite'
+    default_args = args,
+    description = 'This is our first dag that wewrite',
+    start_date = datetime(2024, 10, 27, 2),
+    scheduler_interval = '@daily'
+
 ) as dag:
-    pass
+    task1 = BashOperator(
+        task_id = 'first_task',
+        bash_command = "echo hellow world, this is our first task!"
+    )
+
+    task1
