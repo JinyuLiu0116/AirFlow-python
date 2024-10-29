f1caf376785b5e65425478ef8d71537606c86913
Jinyu Liu <ojin0116@gmail.com>
2024-10-28 20:23:41 -0400

    go

--- AirFlow Foundation/repeatCode/firstDAG#2.py
+++ AirFlow Foundation/repeatCode/firstDAG#2.py
@@ -10,5 +10,8 @@ args = {
 with DAG(
     dag_id = "repeat_dag_untill_familiar",
     default_args = args,
+    description = "practicing will make different!",
+    start_date = datetime(2024, 10, 28, 6),
+    schedule_interal = '@daily'
 ) as dag:
     pass
