b55b89b66c585a39b445c56cc3a139065934ce12
Jinyu Liu <ojin0116@gmail.com>
2024-10-30 22:28:40 -0400

    go

--- AirFlow Foundation/deleteAndAddColumn.py
+++ AirFlow Foundation/deleteAndAddColumn.py
@@ -19,5 +19,18 @@ new_schema.append(bigquery.SchemaField("permalink2", "STRING", mode="NULLABLE"))
 # assign the updated schema to the bigquery_table
 bigquery_table.schema = new_schema
 
-#make an API requerst to add columns
+#make an API requerst to add columns 
 client.update_table(bigquery_table, ['schema'])
+
+#delete columns
+query_job = client.query("""
+    ALTER TABLE Staging.bigquery_copy
+    DROP COLUMN IF EXISTS saved2,
+    DROP COLUMN IF EXISTS permalink2;
+""")
+
+while query_job.state != 'DONE':
+    print('Waiting for job to finish...')
+    time.sleep(3)
+    query_job.reload()
+print(query_job.result())
