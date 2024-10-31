9da589c1d63ae65c103db7e947eaccca909b3d4d
Jinyu Liu <ojin0116@gmail.com>
2024-10-30 22:22:43 -0400

    go

--- AirFlow Foundation/deleteAndAddColumn.py
+++ AirFlow Foundation/deleteAndAddColumn.py
@@ -15,3 +15,9 @@ new_schema = original_schema[:] #creates a copy of the schema
 
 new_schema.append(bigquery.SchemaField("saved2", "BOOLEAN", mode="NULLABLE"))
 new_schema.append(bigquery.SchemaField("permalink2", "STRING", mode="NULLABLE"))
+
+# assign the updated schema to the bigquery_table
+bigquery_table.schema = new_schema
+
+#make an API requerst to add columns
+client.update_table(bigquery_table, ['schema'])
