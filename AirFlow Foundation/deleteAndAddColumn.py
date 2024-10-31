523eb8591a000b13b4b51e7da9bcd01f90763af2
Jinyu Liu <ojin0116@gmail.com>
2024-10-30 22:18:07 -0400

    update code

--- AirFlow Foundation/deleteAndAddColumn.py
+++ AirFlow Foundation/deleteAndAddColumn.py
@@ -3,6 +3,15 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
-dataset_ref = bigquery.DatasetReference(client.project, 'Staging')
-table_ref = bigquery.TableReference(dataset_ref, 'bigquery_copy')
+dataset_ref = bigquery.DatasetReference(client.project, 'Staging')#'dataset id'
+table_ref = bigquery.TableReference(dataset_ref, 'bigquery_copy')#'table name'
 bigquery_table = client.get_table(table_ref)
+
+#get a snapshot of the table schema
+original_schema = bigquery_table.schema # schema atribut provids a list
+
+# add colums
+new_schema = original_schema[:] #creates a copy of the schema
+
+new_schema.append(bigquery.SchemaField("saved2", "BOOLEAN", mode="NULLABLE"))
+new_schema.append(bigquery.SchemaField("permalink2", "STRING", mode="NULLABLE"))
