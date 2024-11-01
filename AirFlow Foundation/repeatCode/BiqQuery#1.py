94d3842e783cdb939938d7bfac4e229692764519
Jinyu Liu <ojin0116@gmail.com>
2024-10-31 20:52:34 -0400

    go

--- AirFlow Foundation/repeatCode/BiqQuery#1.py
+++ AirFlow Foundation/repeatCode/BiqQuery#1.py
@@ -6,7 +6,8 @@ client = bigquery.Client()
 dataset_id = 'Staging'
 table_id = 'bigquery_copy'
 
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-
+dataset_ref = bigquery.DatasetReference(client.project, dataset_id)
+table_ref = bigquery.TableReference(dataset_ref, table_id)
 bigquery_table = client.get_table(table_ref)
+
+original_schema = bigquery_table.schema
