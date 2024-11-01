0c9ce9e160606211973d2606f4de7c587ab18bdf
Jinyu Liu <ojin0116@gmail.com>
2024-10-31 20:53:29 -0400

    go

--- AirFlow Foundation/repeatCode/BiqQuery#1.py
+++ AirFlow Foundation/repeatCode/BiqQuery#1.py
@@ -11,3 +11,9 @@ table_ref = bigquery.TableReference(dataset_ref, table_id)
 bigquery_table = client.get_table(table_ref)
 
 original_schema = bigquery_table.schema
+
+new_schema = original_schema[:]
+new_schema.append(bigquery.SchemaField("saved2", "BOOLEAN", mode="NULLABLE"))
+new_schema.append(bigquery.SchemaField("permalink2", "STRING", mode="NULLABLE"))
+
+bigquery_table.schema = new_schema
