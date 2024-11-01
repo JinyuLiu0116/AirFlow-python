a8739fd33fd9d79a39a33dd164af8e7ca4c54bbd
Jinyu Liu <ojin0116@gmail.com>
2024-10-31 20:54:22 -0400

    go

--- AirFlow Foundation/repeatCode/BiqQuery#1.py
+++ AirFlow Foundation/repeatCode/BiqQuery#1.py
@@ -17,3 +17,18 @@ new_schema.append(bigquery.SchemaField("saved2", "BOOLEAN", mode="NULLABLE"))
 new_schema.append(bigquery.SchemaField("permalink2", "STRING", mode="NULLABLE"))
 
 bigquery_table.schema = new_schema
+
+bigquery_table = client.update_table(bigquery_table, ["schema"])
+
+query_job = client.query("""
+    ALTER TABLE `{}.{}`
+    DROP COLUMN IF EXISTS saved2,
+    DROP COLUMN IF EXISTS permalink2;
+""".format(dataset_id, table_id))
+
+while query_job.state != 'DONE':
+    print('Waiting for job to finish...')
+    time.sleep(3)
+    query_job.reload() 
+
+print("Columns deleted successfully.")
