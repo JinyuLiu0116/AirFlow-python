252cf3e99082a458621d3c6ce2545eadb4f6642c
Jinyu Liu <ojin0116@gmail.com>
2024-11-04 21:53:43 -0500

    go

--- AirFlow Foundation/create_dag_with_operator.py
+++ AirFlow Foundation/create_dag_with_operator.py
@@ -7,6 +7,14 @@ args = {
     'retry_delay' : timedelta(minutes= 2)
 }
 
-with DAG(
 
+
+
+
+with DAG(
+    default_args = args,
+    dag_id = 'our_dag_with_python_operator_v01',
+    description = 'Our first dag using python operator',
+    start_date = datetime(2021, 10, 6),
+    schedule_interval = '@daily'
 ) as dag:
