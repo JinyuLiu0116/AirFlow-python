b43a7549fab263bda6a8bc62a250c69292fd4e41
Jinyu Liu <ojin0116@gmail.com>
2024-11-14 18:02:08 -0500

    update

--- AirFlow Foundation/practicetCode/create_dag_v02.py
+++ AirFlow Foundation/practicetCode/create_dag_v02.py
@@ -7,3 +7,26 @@ args = {
     'retries': 3,
     'retry_delay': timedelta(minutes = 2)
 }
+
+def getName(ti):
+    ti.xcom_push(key = 'first_name', value = 'Jinyu')
+    ti.xcon_push(key = 'last_name', value = 'Liu')
+
+def getAge(ti):
+    ti.xcon_push(key = 'age', value = 35)
+
+with DAG(
+    default_args = args,
+    start_date = datetime.now(),
+    schedule_interval = None
+) as dag:
+    task1 = PythonOperator(
+        task_id = 'getName',
+        python_callable = getName
+    )
+    task2 = PythonOperator(
+        task_id = 'getAge',
+        python_callable = getAge
+    )
+
+    
