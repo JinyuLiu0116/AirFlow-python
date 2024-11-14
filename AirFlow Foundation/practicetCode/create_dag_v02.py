bd68d79ac1c060f0fd33b93aa4af29fe72553004
Jinyu Liu <ojin0116@gmail.com>
2024-11-14 18:06:14 -0500

    update

--- AirFlow Foundation/practicetCode/create_dag_v02.py
+++ AirFlow Foundation/practicetCode/create_dag_v02.py
@@ -15,6 +15,12 @@ def getName(ti):
 def getAge(ti):
     ti.xcon_push(key = 'age', value = 35)
 
+def greed(ti):
+    first_name = ti.xcom_pull(task_ids='getName', key = 'first_name')
+    last_name = ti.xcom_pull(task_ids = 'getName', key = 'last_name')
+    age = ti.xcom_pull(task_ids = 'getAge', key = 'age')
+    print("Hello World! My name is %s %s, I am %s years old", first_name, last_name, age)
+
 with DAG(
     default_args = args,
     start_date = datetime.now(),
@@ -28,5 +34,10 @@ with DAG(
         task_id = 'getAge',
         python_callable = getAge
     )
+    task3 = PythonOperator(
+        task_id = 'greed',
+        python_callable = greed
+    )
+    [task1, task2] >> task3
 
     
